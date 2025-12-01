582c6a754349c6398df0251d70eb4c71b2daf3a7
fasniefjn <144859774+fasniefjn@users.noreply.github.com>
2025-12-01 15:33:19 +0700

    matikan koneksi

--- connection.js
+++ connection.js
@@ -1,19 +1,25 @@
 // db/connection.js
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "dana_kas",
-});
+let db;
 
-db.connect((err) => {
-  if (err) {
-    console.error("MySQL connection error:", err);
-    process.exit(1);
-  }
-  console.log("Connected to MySQL database.");
-});
+try {
+  db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "dana_kas",
+  });
+
+  db.connect((err) => {
+    if (err) {
+      console.warn("Skipping MySQL connection during Jenkins build:", err.message);
+      return;
+    }
+    console.log("Connected to MySQL database.");
+  });
+} catch (err) {
+  console.warn("MySQL connection skipped:", err.message);
+}
 
 module.exports = db;
